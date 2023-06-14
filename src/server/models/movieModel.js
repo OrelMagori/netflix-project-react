8f8de1016960c0b0637e032f602718560752a12f
IdanYaron <idanyaron05@gmail.com>
2023-06-14 21:00:56 +0300

    Create a server side to movies array

---
+++ src/server/models/movieModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema(
+  {
+    synopsis: {
+      type: String,
+      required: true,
+    },
+    director: {
+        type: String,
+        required: true,
+    },
+    actors: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Movie", movieSchema);
---
+++ src/server/routes/movies.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const  {getAllMovies,deleteMovie} = require("../controllers/movieController");
+
+const router = express.Router();
+
+router.get("/", getAllMovies);
+
+router.delete("/delete", deleteMovie); 
+
+module.exports = router;
